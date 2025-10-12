2476ac3b310991f97719592cadc00327c90e385d
Michał Romaszewski <m.romaszewski@hotmail.com>
2025-10-12 13:42:18 +0200

    Comments for main.js

--- src/main/main.js
+++ src/main/main.js
@@ -2,20 +2,24 @@ const { app } = require('electron');
 const { createWindow } = require('./core/app');
 const { registerVaultIpcHandlers } = require('./ipc/vaultIpc');
 
+// Import and create new VaultManager
 const VaultManager = require('./utils/vaultManager');
 global.vaultMgr = new VaultManager();
 
 app.whenReady().then(() => {
+    // Create window
     const mainWindow = createWindow();
 
+	// Register IPC event handlers
     registerVaultIpcHandlers(mainWindow);
-    registerVaultIpcHandlers();
 });
 
+// Handle windows close as app exit 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
 
+// Handle app exit
 app.on('will-quit', () => {
     const { closeCurrentDB } = require('./utils/db');
     closeCurrentDB();
