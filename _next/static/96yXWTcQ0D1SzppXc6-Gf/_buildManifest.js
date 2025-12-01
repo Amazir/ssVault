35361ecb27f9a61c270458255011053973e7d7c9
mromasze <mromasze@users.noreply.github.com>
2025-12-01 14:32:19 +0000

    deploy: e6132a5e789ee073b0587f5110bed6f8c9a3c3b8

--- _next/static/96yXWTcQ0D1SzppXc6-Gf/_buildManifest.js
+++ _next/static/96yXWTcQ0D1SzppXc6-Gf/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ssVault//_next/:path+",
+        "destination": "/ssVault/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
